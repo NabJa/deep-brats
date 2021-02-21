d8f4fb6fdfbc565eff4d2bc7a45d1cf652694f57
Nabil <nabil.jabareen@gmail.com>
2021-02-21 12:13:24 +0100

    Update notebook and lint

--- uncertainty/data_generator/data.py
+++ uncertainty/data_generator/data.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
+import h5py
 import click
 
 DATA_PATH = Path("../../data")
 
-BRATS_IMAGES = Path(DATA_PATH/"BraTS2020_training_data")
-SURVIVAL_INFO = Path(DATA_PATH/"survival_info.csv")
-META_DATA = Path(DATA_PATH/"meta_data.csv")
+BRATS_IMAGES = Path(DATA_PATH / "BraTS2020_training_data")
+SURVIVAL_INFO = Path(DATA_PATH / "survival_info.csv")
+META_DATA = Path(DATA_PATH / "meta_data.csv")
 
 
 @click.command()
@@ -15,9 +17,10 @@ META_DATA = Path(DATA_PATH/"meta_data.csv")
 def preprocess_kaggle_data(survival_info_path, meta_data_path):
 
     # Process survival info
-
     survival_info = pd.read_csv(Path(survival_info_path))
-    survival_info.loc[survival_info.Survival_days == "ALIVE (361 days later)", "Survival_days"] = 361
+    survival_info.loc[
+        survival_info.Survival_days == "ALIVE (361 days later)", "Survival_days"
+    ] = 361
     survival_info.Survival_days = survival_info.Survival_days.astype(int)
     print(f"Saving processed survival info to {survival_info_path}")
     survival_info.to_csv(survival_info_path)
@@ -41,11 +44,11 @@ class BratsData:
 
         for i, row in df.iterrows():
             path = Path(data_path).joinpath(row.slice_path)
-            hf = h5py.File(path, 'r')
+            hf = h5py.File(path, "r")
             img = np.array(hf.get("image"))
-            mask = np.array(hf.get("mask"))s
+            mask = np.array(hf.get("mask"))
             yield {"image": img, "mask": mask, "slice": row.slice}
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     preprocess_kaggle_data()
