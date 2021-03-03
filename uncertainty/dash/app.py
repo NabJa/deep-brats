d989776de7f9348c98817868a69972f1178ba324
Nabil <nabil.jabareen@gmail.com>
2021-03-03 18:46:57 +0100

    Add 3d plot to dashboard

--- uncertainty/dash/app.py
+++ uncertainty/dash/app.py
@@ -1,44 +1,25 @@
+import pandas as pd
+from pathlib import Path
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.express as px
 
-import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        step=None
-    )
-])
+from uncertainty.visualisation.interactive import plot_volume_3d
+from uncertainty.data import data
 
 
-@app.callback(
-    Output('graph-with-slider', 'figure'),
-    Input('year-slider', 'value'))
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
+# Basic MRI plot
 
-    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                     size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
+meta_data = pd.read_csv(Path("../../data/meta_data.csv"))
 
-    fig.update_layout(transition_duration=500)
+brain, bmask = data.get_volume(1, meta_data)
+t1, t1gd, t2, flair = brain[..., 0], brain[..., 1], brain[..., 2], brain[..., 3]
 
-    return fig
+fig = plot_volume_3d(t1)
+
+app = dash.Dash()
+app.layout = html.Div([dcc.Graph(figure=fig)])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
---
+++ uncertainty/visualisation/interactive.py
@@ -0,0 +1,98 @@
+import plotly.graph_objects as go
+import numpy as np
+
+
+def plot_volume_3d(volume):
+    """Generate interactive 3D plot of volume with slider."""
+
+    nslices, width, height = volume.shape
+    vmin, vmax = volume.min(), volume.max()
+    base_mat = np.ones((height, width))
+    nframes = (nslices - 1) / 10
+
+    def _frame_args(duration):
+        return {
+            "frame": {"duration": duration},
+            "mode": "immediate",
+            "fromcurrent": True,
+            "transition": {"duration": duration, "easing": "linear"},
+        }
+
+    fig = go.Figure(
+        frames=[
+            go.Frame(
+                data=go.Surface(
+                    z=(nslices - i) * base_mat,
+                    surfacecolor=volume[nslices - (i + 1)],
+                    cmin=vmin,
+                    cmax=5,
+                ),
+                name=f"Frame {i}",
+            )
+            for i in range(nslices)
+        ]
+    )
+
+    # Add data to be displayed before animation starts
+    fig.add_trace(
+        go.Surface(
+            z=nslices * base_mat,
+            surfacecolor=volume[nslices - 1],
+            colorscale="Gray",
+            cmin=vmin,
+            cmax=5,
+            colorbar=dict(thickness=20, ticklen=4),
+        )
+    )
+
+    sliders = [
+        {
+            "pad": {"b": 10, "t": 60},
+            "len": 0.9,
+            "x": 0.1,
+            "y": 0,
+            "steps": [
+                {
+                    "args": [[frame.name], _frame_args(0)],
+                    "label": str(i),
+                    "method": "animate",
+                }
+                for i, frame in enumerate(fig.frames)
+            ],
+        }
+    ]
+
+    # Layout
+    fig.update_layout(
+        title="Slices in MRI data",
+        width=750,
+        height=750,
+        scene=dict(
+            zaxis=dict(range=[-1, nslices + 1], autorange=False),
+            aspectratio=dict(x=1, y=1, z=1),
+        ),
+        updatemenus=[
+            {
+                "buttons": [
+                    {
+                        "args": [None, _frame_args(50)],
+                        "label": "&#9654;",  # play symbol
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], _frame_args(0)],
+                        "label": "&#9724;",  # pause symbol
+                        "method": "animate",
+                    },
+                ],
+                "direction": "left",
+                "pad": {"r": 10, "t": 70},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }
+        ],
+        sliders=sliders,
+    )
+
+    return fig
