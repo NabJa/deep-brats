3db1f1e2f58596e6dc26d4d94458a89cf2c95c71
NabJa <nabil.jabareen@gmail.com>
2021-03-05 13:54:47 +0100

    Basic dash app styling

--- uncertainty/dash/app.py
+++ uncertainty/dash/app.py
@@ -7,6 +7,9 @@ import dash_html_components as html
 from uncertainty.visualisation.interactive import plot_volume_3d
 from uncertainty.data import data
 
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Basic MRI plot
 
@@ -17,8 +20,16 @@ t1, t1gd, t2, flair = brain[..., 0], brain[..., 1], brain[..., 2], brain[..., 3]
 
 fig = plot_volume_3d(t1)
 
-app = dash.Dash()
-app.layout = html.Div([dcc.Graph(figure=fig)])
+app.layout = html.Div(
+    style={"height": "95vh"},
+    children=[
+        html.H1(
+            "3D Plot of MRI image",
+            style={"textAlign": "center"},
+        ),
+        html.Center(dcc.Graph(figure=fig)),
+    ],
+)
 
 
 if __name__ == "__main__":
